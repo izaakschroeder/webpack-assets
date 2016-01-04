49429480ccf6a09bf65709fd27d807f098460b33
Izaak Schroeder <izaak.schroeder@gmail.com>
2016-01-04 04:26:01 -0800

    Fix pathing issue with extra '/'.
    Off-by-one error previously. Also added a check for the file in addition to the `publicPath`. Shouldn't be any more funny business.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 function ensureDirectory(path) {
-  return path.charAt(path.length) !== '/' ? `${path}/` : path;
+  return path.charAt(path.length - 1) !== '/' ? `${path}/` : path;
+}
+
+function ensureRelative(path) {
+  return path.charAt(0) !== '/' ? path : path.substr(1);
 }
 
 export default function collect({ chunks, publicPath }) {
@@ -12,7 +16,7 @@ export default function collect({ chunks, publicPath }) {
     .sort((a, b) => a.entry === b.entry ? b.id - a.id : b.entry - a.entry)
     .reduce((list, chunk) => list.concat(chunk.files.map(file => {
       return {
-        url: base + file,
+        url: base + ensureRelative(file),
         chunk,
       };
     })), []);
