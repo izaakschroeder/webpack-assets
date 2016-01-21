cbec69ce9dbc1caa12f399f5c23ea4af8e2c1824
Izaak Schroeder <izaak.schroeder@gmail.com>
2016-01-20 21:06:48 -0800

    Make asset collection smarter with new properties.
    There is now both `contentType` and `eTag` available on the resulting assets. Additionally, the mechanism by which asset properties are generated is now configurable, and many more properties can be added easily.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 
+import { lookup } from 'mime-types';
+import path from 'path';
+
 function ensureDirectory(path) {
   return path.charAt(path.length - 1) !== '/' ? `${path}/` : path;
 }
@@ -7,17 +10,53 @@ function ensureRelative(path) {
   return path.charAt(0) !== '/' ? path : path.substr(1);
 }
 
-export default function collect({ chunks, publicPath }) {
-  const base = ensureDirectory(publicPath);
+function contentType(asset) {
+  return {
+    ...asset,
+    contentType: lookup(asset.file),
+  };
+}
 
+function name(asset) {
+  const name = asset.name || path.basename(asset.file);
+  return {
+    ...asset,
+    name: name,
+  };
+}
+
+function url(asset, { publicPath }) {
+  const base = ensureDirectory(publicPath);
+  return {
+    ...asset,
+    url: base + ensureRelative(asset.file),
+  };
+}
+
+function hash(asset) {
+  return {
+    ...asset,
+    hash: asset.chunk.hash,
+  };
+}
+
+const props = [
+  name,
+  url,
+  contentType,
+  hash,
+];
+
+export default function collect(stats) {
+  const { chunks } = stats;
   // Order the chunks so commons chunks come first.
   return chunks
     .slice()
     .sort((a, b) => a.entry === b.entry ? b.id - a.id : b.entry - a.entry)
     .reduce((list, chunk) => list.concat(chunk.files.map(file => {
-      return {
-        url: base + ensureRelative(file),
+      return props.reduce((result, prop) => prop(result, stats), {
+        file,
         chunk,
-      };
+      });
     })), []);
 }
--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -34,3 +34,30 @@ it('should handle odd paths', () => {
   expect(assets).to.have.length(2);
   expect(assets[0]).to.have.property('url', '/foo/bar.js');
 });
+
+it('should generate `contentType`', () => {
+  const assets = collect({
+    publicPath: '/foo/',
+    chunks: [ {
+      id: 5,
+      files: [ '/bar.js' ],
+    } ],
+  });
+  expect(assets).to.be.an.instanceof(Array);
+  expect(assets).to.have.length(1);
+  expect(assets[0]).to.have.property('contentType', 'application/javascript');
+});
+
+it('should generate `hash`', () => {
+  const assets = collect({
+    publicPath: '/foo/',
+    chunks: [ {
+      id: 5,
+      hash: '123abc',
+      files: [ '/bar.js' ],
+    } ],
+  });
+  expect(assets).to.be.an.instanceof(Array);
+  expect(assets).to.have.length(1);
+  expect(assets[0]).to.have.property('hash', '123abc');
+});
