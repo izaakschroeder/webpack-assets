be56da9e2b03030cde3b893d0b16925c1e9c7f96
Izaak Schroeder <izaak.schroeder@gmail.com>
2016-01-26 16:53:39 -0800

    Use `assets` instead of `chunks`.
    There are some cases when assets are generated without chunks, e.g. using `file-loader`. In those cases the previous result of `webpack-assets` did not correctly include them. Now it reads `stats.assets` for the information. It should be kind of API compatible, though the `hash` calculation is different, `name` is now the complete name (what `file` was before) and the input requests `stats.assets` to be present.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-
 import { lookup } from 'mime-types';
-import path from 'path';
 
 function ensureDirectory(path) {
   return path.charAt(path.length - 1) !== '/' ? `${path}/` : path;
@@ -17,11 +15,10 @@ function contentType(asset) {
   };
 }
 
-function name(asset) {
-  const name = asset.name || path.basename(asset.file);
+function file(asset) {
   return {
     ...asset,
-    name: name,
+    file: asset.name,
   };
 }
 
@@ -33,30 +30,44 @@ function url(asset, { publicPath }) {
   };
 }
 
-function hash(asset) {
+function hash(asset, stats) {
   return {
     ...asset,
-    hash: asset.chunk.hash,
+    hash: stats.hash,
   };
 }
 
 const props = [
-  name,
+  file,
   url,
   contentType,
   hash,
 ];
 
+const entry = (stats, asset) => {
+  return asset.chunks.some(id => stats.chunks[id].entry);
+};
+
+const index = (stats, asset) => {
+  return Math.min(...asset.chunks);
+};
+
+const comparator = (stats) => (a, b) => {
+  const eA = entry(stats, a);
+  const eB = entry(stats, b);
+
+  if (eA && eB) {
+    return index(stats, a) - index(stats, b);
+  }
+  return eB - eA;
+};
+
 export default function collect(stats) {
-  const { chunks } = stats;
   // Order the chunks so commons chunks come first.
-  return chunks
+  return stats.assets ? stats.assets
     .slice()
-    .sort((a, b) => a.entry === b.entry ? b.id - a.id : !!b.entry - !!a.entry)
-    .reduce((list, chunk) => list.concat(chunk.files.map(file => {
-      return props.reduce((result, prop) => prop(result, stats), {
-        file,
-        chunk,
-      });
-    })), []);
+    .sort(comparator(stats))
+    .map(asset => {
+      return props.reduce((result, prop) => prop(result, stats), asset);
+    }) : [];
 }
